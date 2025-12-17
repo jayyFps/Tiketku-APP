9cec77b57e7909b77a3fb76fadfbfb0f467e9165
ari riyanto <sanditiya373@gmail.com>
2025-12-17 07:20:33 +0700

    Debug: Show detailed error message on delete failure

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files
